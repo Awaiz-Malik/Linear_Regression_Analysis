94aaa83511d5452277d6818cf7dfb30289f34d9d
Awaiz-Malik <115945150+Awaiz-Malik@users.noreply.github.com>
2023-03-16 14:21:13 +0500

    Update MainForm.cpp

--- Regression Analysis/MainForm.cpp
+++ Regression Analysis/MainForm.cpp
@@ -1,10 +1,10 @@
-#include "MainForm.h"
+#include "MainForm.h" // Instead of MainForm use name of your cpp file
 
 int main(array<System::String^>^ args)
 {
     System::Windows::Forms::Application::EnableVisualStyles();
     System::Windows::Forms::Application::SetCompatibleTextRenderingDefault(false);
-    Forms::MainForm form;
+    Forms::MainForm form; // Instead of MainForm use name of your cpp file and instead of Forms use the name of your Project
     System::Windows::Forms::Application::Run(% form);
     return 0;
 }
